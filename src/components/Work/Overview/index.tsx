61ee663f7006e403594e7f501f768d9423c1ed25
Julian Moscoloni <jmoscoloni@gmail.com>
2026-03-18 16:48:17 -0300

    updates

--- src/components/Work/Overview/index.tsx
+++ src/components/Work/Overview/index.tsx
@@ -40,9 +40,9 @@ const projects = [
   },
   {
     id: 6,
-    image: '/Images/HSP HOUSE - YARD.jpg',
+    image: '/Images/HSP HOUSE.jpg',
     title: 'HSP House',
-    route: 'hsp-house-2',
+    route: 'hsp-house',
     paragraph: 'A bold expression of raw concrete and geometry captured through virtual lenses.',
     subtitle: 'E2M architects',
     slug: 'nicola-copy-3'
