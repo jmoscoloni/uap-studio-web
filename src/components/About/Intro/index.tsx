aa30f3c079fdc9f4d59a7cc146c3f95f0f6aad97
Julian Moscoloni <jmoscoloni@gmail.com>
2026-03-22 13:07:50 -0300

    update index

--- src/components/About/Intro/index.tsx
+++ src/components/About/Intro/index.tsx
@@ -47,7 +47,7 @@ const Intro = () => {
               </svg>
             </div>
           </Button>
-          <Button href="https://www.linkedin.com/in/uap-studio-b913143b0/">
+          <Button href="https://www.linkedin.com/company/uap-studio/">
             <div className="h-4 w-4">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
