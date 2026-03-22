e5966ae86139f92090e148124621d550216145a2
Julian Moscoloni <jmoscoloni@gmail.com>
2026-03-22 10:24:05 -0300

    hsp house project updates

--- src/app/(pages)/[locale]/work/[uid]/page.tsx
+++ src/app/(pages)/[locale]/work/[uid]/page.tsx
@@ -76,7 +76,7 @@ export default async function TestPage({ params }: LocaleWithUidPageProps) {
       title: 'HSP House',
       subtitle: 'E2M architects',
       overview: 'A bold expression of raw concrete and geometry captured through virtual lenses.',
-      images: ['/Images/HSP HOUSE - Image 1.jpg']
+      images: ['/Images/HSP HOUSE - YARD.jpg', '/Images/HSP HOUSE - Image 1.jpg']
     }
   ];
 
